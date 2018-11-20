79de78ba83662ef44534dbe2e082dc76735cb8ad
Soul Evans <soulevans07@gmail.com>
2018-11-20 22:29:41 +0100

    task controller update

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const entities = require('html-entities').AllHtmlEntities;
 const Task = require('../models/taskModel');
 
+exports.list = async function (req, res, next) {
+    let tasks = await Task.find().exec();
+
+    return res.status(200).send(tasks);
+};
+
 exports.new = async function (req, res, next) {
     let task = new Task({
         name: entities.encode(req.body.name),
@@ -13,30 +19,39 @@ exports.new = async function (req, res, next) {
     return res.status(200).send(task);
 };
 
-exports.update = async function (req, res, next) {
-    console.log(req.body);
-    try {
-        id = new mongoose.Types.ObjectId(req.params.id);
-    } catch (err) {
-        let newErr = new Error('Invalid user id: ' + req.params.uid + ' (Caused by: ' + err.message + ')');
-        return res.status(400).send(newErr);
+exports.get = async function (req, res, next) {
+    let task = await Task.findById(req.params.id).exec();
+
+    if (!task) {
+        return res.status(400).send('No task with id: ' + req.params.id);
     }
 
-    let task = await Task.findOne({ _id: id }).exec();
-
-    if (task) {
-        task.name = req.body.name;
-        task.is_done = req.body.is_done;
-
-        task = await task.save();
-
-        return res.status(200).send(task);
-    }
-    return res.status(500).send('Task is null');
+    return res.status(200).send(task);
 };
 
-exports.list = async function (req, res, next) {
-    let tasks = await Task.find().exec();
+exports.update = async function (req, res, next) {
+    let task = await Task.findById(req.params.id).exec();
 
-    return res.status(200).send({ list: tasks });
+    if (!task) {
+        return res.status(400).send('No task with id: ' + req.params.id);
+    }
+
+    task.name = req.body.name;
+    task.state = req.body.state;
+
+    task = await task.save();
+
+    return res.status(200).send(task);
+};
+
+exports.delete = async function (req, res, next) {
+    let task = await Task.findById(req.params.id).exec();
+
+    if (!task) {
+        return res.status(400).send('No task with id: ' + req.params.id);
+    }
+
+    await task.delete();
+
+    return res.status(200).send();
 };
